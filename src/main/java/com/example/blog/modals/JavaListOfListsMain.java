391c71c516bcd7700279e5b2aa6da5705d67f6bb
Chelsea <chelsea.e.rolland@gmail.com>
2020-12-11 16:28:54 -0600

    testing out the nested lists in java - it does work

---
+++ src/main/java/com/example/blog/modals/JavaListOfListsMain.java
@@ -0,0 +1,27 @@
+package com.example.blog.modals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaListOfListsMain {
+    public static void main(String[] args) {
+        List<ArrayList<String>> listOfLists = new ArrayList<ArrayList<String>>();
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Delhi");
+        list1.add("Mumbai");
+        listOfLists.add(list1);
+
+        ArrayList<String> anotherList = new ArrayList<String>();
+
+        anotherList.add("Beijing");
+        anotherList.add("Shanghai");
+        listOfLists.add(anotherList);
+
+        listOfLists.forEach((list)  ->
+                {
+                    //list.forEach((city)->System.out.println(city));
+                    System.out.println("list = " + list);
+                }
+        );
+    }
+}
